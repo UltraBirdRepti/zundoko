029b8fd8833a83dca1236e20f40472ed065c06d5
UltraBirdRepti <ultrabirdrepti@gmail.com>
2017-04-09 15:31:03 +0900

    遊んでみた

--- zundoko.rb
+++ zundoko.rb
@@ -1,25 +1,32 @@
-def zundoko
-  array = [] 
-  loop do
-    # ランダム値生成。２で割った余りでズン or ドコを出し分けている
-    if rand(10) % 2 == 0
-      print 'ズン'
-      # 2で割れればズンを入れる。<< は破壊的。
-      array << 'ズン'
-    else
-      print 'ドコ'
-      array << 'ドコ'
-    end 
-
-    if !array[-5..-1].nil?
-      if array[-5..-1].join == 'ズンズンズンズンドコ'
-        print 'キ・ヨ・シ!'
-        break;
+class String
+  def zundoko
+    if self != '氷川きよし'
+      print 'あなたは氷川きよしではありません'
+      return
+    end
+    
+    array = []
+    loop do
+      if rand(2) % 2 == 0
+        print 'ズン' 
+        array << 'ズン'
       else
-        array.shift
+        print 'ドコ'
+        array << 'ドコ'
+      end 
+
+      if !array[-5..-1].nil?
+        if array[-5..-1].join == 'ズンズンズンズンドコ' 
+          print 'キ・ヨ・シ!'
+          break
+        else
+          array.shift
+        end
       end
     end
   end
 end
 
-zundoko()
+# 黒魔術でStringを拡張してzundokoメソッドを追加
+# 'つるの剛士'.zundoko() # => error
+# '氷川きよし'.zundoko() # => zundoko発動!!
